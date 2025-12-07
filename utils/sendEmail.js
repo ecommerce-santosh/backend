d9024b8dff8c6c7802e805ed514ad9721b0cdfcb
Programmer-Santosh <santosh.programming.main@gmail.com>
2025-12-07 18:54:18 +0545

    fytf7fyuyu

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -5,8 +5,8 @@ const sendEmail = async (to, subject, html) => {
     // ✅ Create Transporter
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST || "smtp.gmail.com",
-      port: process.env.SMTP_PORT ? Number(process.env.SMTP_PORT) : 587,
-      secure: false, // true for 465, false for other ports
+      port: process.env.SMTP_PORT ? Number(process.env.SMTP_PORT) : 465,
+      secure: true, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
